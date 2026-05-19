21953afe54bd1bc869d32cc6be929297d98bf314
ValdemarMagnusson <vavva.magnusson@gmail.com>
2026-05-19 15:01:14 +0200

    Slightly increase nav ValoraLogo size

--- src/components/ValoraLogo.tsx
+++ src/components/ValoraLogo.tsx
@@ -4,7 +4,7 @@ interface ValoraLogoProps {
 }
 
 const sizeClass = {
-  small: "h-10 w-auto",
+  small: "h-12 w-auto",
   large: "h-20 w-auto",
 } as const;
 
@@ -13,8 +13,8 @@ export const ValoraLogo = ({ size = "small", className = "" }: ValoraLogoProps) 
     <img
       src="/logo_black.svg"
       alt=""
-      width={size === "large" ? 76 : 38}
-      height={size === "large" ? 80 : 40}
+      width={size === "large" ? 76 : 46}
+      height={size === "large" ? 80 : 48}
       className={`shrink-0 object-contain ${sizeClass[size]} ${className}`}
       decoding="async"
     />
